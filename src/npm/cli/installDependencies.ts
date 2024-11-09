a051444c5c340540cfabba3f75cc3d59c7386270
Mark Chris Levy <levy@codeandmore.pl>
2024-11-09 22:25:56 +0100

    commit

--- src/npm/cli/installDependencies.ts
+++ src/npm/cli/installDependencies.ts
@@ -12,11 +12,12 @@ if (process.cwd().indexOf("node_modules") < 0 && type) {
 
     const rootDir = path.resolve("./");
     const pckg = fs.readJsonSync("package.json");
-    const dependencies = pckg[`${type}Dependencies`];
+    const dependencies: {[key: string]: string} = pckg[`${type}Dependencies`];
 
     if (Object.keys(dependencies).length) {
 
-        for (const d of Object.keys(dependencies)) {
+        for (const [d, v] of Object.entries(dependencies)) {
+            const n = type === "optional" ? d : (v.startsWith("file:") || v.startsWith("github:") ? v : d);
             child_process.execSync(`npm install ${d} --no-save`, {stdio: "inherit"});
         }
     }
